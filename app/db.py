3869196a038f624d4caa107e4b7f6354918c51a9
RemiSlanina <remislanina@gmail.com>
2026-05-29 15:29:56 +0200

    Add basic search and validation

--- app/db.py
+++ app/db.py
@@ -12,6 +12,13 @@ def get_connection():
         password=os.getenv("DB_PASSWORD"),
     )
 
+# validation helper function
+def is_valid_scale_value(value, field_name):
+    if value < 0 or value > 10:
+        raise ValueError(f"{field_name} must be between 0 and 10")
+
+# create, get, update entries
+
 def create_entry(
         energy_level,
         pain_level,
@@ -19,6 +26,9 @@ def create_entry(
         food_tolerance,
         note
 ):
+    is_valid_scale_value(energy_level, "energy_level")
+    is_valid_scale_value(pain_level, "pain_level")
+    is_valid_scale_value(sensory_load, "sensory_load")
     conn = get_connection()
     cur = conn.cursor()
 
@@ -63,6 +73,7 @@ def update_entry(
         entry_id,
         energy_level
 ):
+    is_valid_scale_value(energy_level, "energy_level")
     conn = get_connection()
     cur = conn.cursor()
     cur.execute(
@@ -91,4 +102,73 @@ def delete_entry(entry_id):
     conn.commit()
     cur.close()
     conn.close()
+
+# search entries
+
+def search_notes(keyword):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        SELECT * FROM health_entries
+        WHERE note ILIKE %s 
+        ORDER BY timestamp DESC
+        """,
+        (f"%{keyword}%",)
+    )
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+def search_food_tolerance(keyword):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT * FROM health_entries
+        WHERE food_tolerance ILIKE %s 
+        ORDER BY timestamp DESC
+        """,
+        (f"%{keyword}%",)
+    )
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+def get_entries_by_energy(level):
+    is_valid_scale_value(level, "energy_level")
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT * FROM health_entries 
+        WHERE energy_level = %s
+        ORDER BY energy_level DESC
+        """,
+        (level,)
+    )
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+def get_entries_by_pain(level):
+    is_valid_scale_value(level, "pain_level")
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT * FROM health_entries 
+        WHERE pain_level = %s
+        ORDER BY pain_level DESC
+        """,
+        (level,)
+    )
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
 
--- app/main.py
+++ app/main.py
@@ -1,26 +1,59 @@
-
-from db import create_entry, get_entries, update_entry, delete_entry
+from db import (create_entry, get_entries, update_entry, delete_entry, search_notes, search_food_tolerance,
+                get_entries_by_energy, get_entries_by_pain)
 
 def main():
-    create_entry(
-        4,5,7,"rice tolerated", "noise sensitivity high"
-    )
+    try:
+        create_entry(
+            4,5,7,"toast tolerated", "noise sensitivity high"
+        )
 
-    update_entry(3, 9)
-    entries = get_entries()
+        update_entry(3, 9)
+        entries = get_entries()
 
-    for entry in entries:
-        print(entry)
-    print("\n")
+        for entry in entries:
+            print(entry)
+        print("\n")
 
-    latest_entry_id = entries[0][0]
-    print(latest_entry_id)
-    delete_entry(latest_entry_id)
-    delete_entry(4)
-    entries = get_entries()
+        latest_entry_id = entries[0][0]
+        print(latest_entry_id)
+        delete_entry(latest_entry_id)
+        delete_entry(4)
+        entries = get_entries()
 
-    for entry in entries:
-        print(entry)
+        for entry in entries:
+            print(entry)
+
+        # search
+        print("\n")
+        note_results = search_notes("noise")
+        print("results with keyword \"noise\"")
+        print_results(note_results)
+
+        print("")
+        food_results = search_food_tolerance("toast")
+        print("results with food-keyword \"toast\"")
+        for f_result in food_results:
+            print(f_result)
+
+        print("")
+        energy_results = get_entries_by_energy(9)
+        print("results with energy = 9")
+        print_results(energy_results)
+
+        print("")
+        pain_results = get_entries_by_pain(9)
+        print("results with pain = 9")
+        print_results(pain_results)
+    except ValueError as e:
+        print(e)
+
+def print_results(results):
+    if len(results) == 0:
+        print("No results")
+        return
+    for result in results:
+        print(result)
+
 
 if __name__ == "__main__":
     main()
