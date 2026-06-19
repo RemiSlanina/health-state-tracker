020d1b3178bf25668559d9e2809483c20bdb5f7a
RemiSlanina <remislanina@gmail.com>
2026-06-19 14:27:26 +0200

    Add mocked test for get_entry

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,37 @@
 from app.db import convert_tuple_into_health_entry, convert_tuples_into_health_entries
 from app.models import HealthEntry
+from unittest.mock import MagicMock, patch
+from app.db import get_entry
 
+def test_get_entry():
+    fake_connection = MagicMock()
+    fake_cursor = MagicMock()
+
+    # `with` calls __enter__(), so we mock the object returned by it.
+    # Without `with`: fake_connection.cursor.return_value = fake_cursor
+    fake_connection.cursor.return_value.__enter__.return_value = fake_cursor
+    fake_connection.__enter__.return_value = fake_connection
+
+    fake_cursor.fetchone.return_value = (
+        123,
+        "2026-06-12",
+        4,
+        5,
+        6,
+        "apples",
+        "forgot taking my meds"
+    )
+
+    with patch("app.db.get_connection", return_value=fake_connection):
+        result = get_entry(123)
+
+    assert result.id == 123
+    assert result.timestamp == "2026-06-12"
+    assert result.energy_level == 4
+    assert result.pain_level == 5
+    assert result.sensory_load == 6
+    assert result.food_tolerance ==  "apples"
+    assert result.note == "forgot taking my meds"
 
 def test_convert_tuple_into_health_entry():
     row  = (
